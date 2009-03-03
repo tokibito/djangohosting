21f8aa81e27a45e22e8167e4af1daab8d5a93e1f
Shinya Okano <xxshss@yahoo.co.jp>
2009-03-04 03:45:33 +0900

    added daemonize option.

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 from wsgiref.simple_server import make_server, WSGIServer
-from wsgi import WSGIHandler
 from SocketServer import ThreadingMixIn
+
+from django.utils.daemonize import become_daemon
+
+from wsgi import WSGIHandler
+
 class ThreadingWSGIServer(ThreadingMixIn, WSGIServer):
     pass
 
@@ -13,6 +17,8 @@ if __name__ == '__main__':
     import sys
     from conf import Settings
     settings = Settings()
+    if settings.server.daemonize:
+        become_daemon()
     if len(sys.argv) == 2:
         runserver(sys.argv[1], 8000)
     elif len(sys.argv) >= 3:
